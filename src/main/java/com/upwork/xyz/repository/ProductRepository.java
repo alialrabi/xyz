643629641f1b181b084df424b6ed5e2166c4692d
elabbasy1995 <elabbasy1995@gmail.com>
2021-10-17 18:14:07 +0200

    more simple query

--- src/main/java/com/upwork/xyz/repository/ProductRepository.java
+++ src/main/java/com/upwork/xyz/repository/ProductRepository.java
@@ -6,13 +6,14 @@ import com.upwork.xyz.security.AuthoritiesConstants;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import com.upwork.xyz.security.AuthoritiesConstants;
 
 import com.upwork.xyz.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long>,  JpaSpecificationExecutor<Product> {
 	
 
-	@Query(value = "SELECT * FROM product p inner join store_products s on p.id = s.product_id WHERE (?#{hasAuthority('ADMIN')}) OR (s.store_id = (select store_id from user u inner join user_authority a on u.id = a.user_id where u.username = ?#{ principal?.username } and a.authority_name = 'EMPLOYEE'))" , nativeQuery = true)
+	@Query(value = "SELECT * FROM product p inner join store_products s on p.id = s.product_id WHERE (?#{hasAuthority('"+AuthoritiesConstants.ADMIN+"')}) OR (s.store_id = (select store_id from user u where u.username = ?#{ principal?.username } and ?#{hasAuthority('"+AuthoritiesConstants.EMPLOYEE+"')}))" , nativeQuery = true)
 	public Set<Product> search();
 
 
